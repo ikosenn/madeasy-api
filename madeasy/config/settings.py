8d103899ae84d3c26dd793e6add2542f6257d999
Ian Kosen <i@kosenn.me>
2016-01-10 21:33:45 +0300

    Add all setup files

--- madeasy/config/settings.py
+++ madeasy/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'madeasy.urls'
+ROOT_URLCONF = 'madeasy.config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'madeasy.wsgi.application'
+WSGI_APPLICATION = 'madeasy.config.wsgi.application'
 
 
 # Database
