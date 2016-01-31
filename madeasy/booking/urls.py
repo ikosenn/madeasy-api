6c46f56704294f1ebf93161d65039db4fcd47046
ikosenn <i@kosenn.me>
2016-02-01 00:44:13 +0300

    Minor edits

--- madeasy/booking/urls.py
+++ madeasy/booking/urls.py
@@ -11,18 +11,18 @@ from .views import (
 
 
 urlpatterns = [
-    url(r'^ticket_types/',
+    url(r'^ticket_types/$',
         TicketTypeListView.as_view(), name='ticket_types_list'),
-    url(r'^ticket_types/(?P<pk>[0-9]+)$',
+    url(r'^ticket_types/(?P<pk>[0-9]+)/$',
         TicketTypeDetailView.as_view(), name='ticket_types_detail'),
 
-    url(r'^booking_status',
+    url(r'^booking_status/$',
         BookingStatusListView.as_view(), name='booking_status_list'),
-    url(r'^booking_status/(?P<pk>[0-9]+)$',
+    url(r'^booking_status/(?P<pk>[0-9]+)/$',
         BookingStatusDetailView.as_view(), name='booking_status_detail'),
 
-    url(r'^bookings/',
+    url(r'^bookings/$',
         BookingListView.as_view(), name='bookings_list'),
-    url(r'^bookings/(?P<pk>[0-9]+)$',
+    url(r'^bookings/(?P<pk>[0-9]+)/$',
         BookingDetailView.as_view(), name='bookings_detail'),
 ]
