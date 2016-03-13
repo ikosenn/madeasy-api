49c3fdf0d3dcbad7857653ab34c6bf020e03740d
ikosenn <i@ikosenn.me>
2016-03-13 12:12:52 +0300

    Add env var for path to static during production

--- madeasy/config/urls.py
+++ madeasy/config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -32,6 +33,5 @@ urlpatterns = [
         include('madeasy.airline.urls', namespace='airline')),
 
     url(r'^api/parser/', include('madeasy.parser.urls', namespace='parser')),
-    url(r'^static/(?P<path>.*)$',
-        'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- playbooks/roles/madeasyapi/templates/madeasyapi.env.sh
+++ playbooks/roles/madeasyapi/templates/madeasyapi.env.sh
@@ -4,6 +4,7 @@ export CSRF_COOKIE_NAME="{{csrf_cookie_name}}"
 export SESSION_COOKIE_NAME="{{session_cookie_name}}"
 export VENV_DIR="{{venv_dir}}"
 export HTTPS_ENABLED="{{ssl_on}}"
+export STATIC_ROOT="{{static_dir}}"
 # a hack for python 3 and boto
 # https://github.com/travis-ci/travis-ci/issues/5246
 export BOTO_CONFIG="/tmp/bogus"
