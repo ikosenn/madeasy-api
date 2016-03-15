b4bc665be6d198be9089819cf3415e2b7b6aa1f9
ikosenn <i@ikosenn.me>
2016-03-15 16:10:26 +0300

    Add default data for airlines and airports
    + Creates models for routes and the route tree

--- madeasy/airline/models.py
+++ madeasy/airline/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
 from madeasy.common.models import AbstractBase
 
 
@@ -8,9 +10,21 @@ class Airport(AbstractBase):
     where the aircraft's destination or origin location
     """
     airport_name = models.CharField(max_length=255)
-    airport_code = models.CharField(max_length=10)
     city = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
+    iata = models.CharField(
+        max_length=3, null=True, blank=True,
+        help_text=(
+            'FAA for airports located in Country'
+            ' United States of America 3-letter IATA code, '
+            'for all other airports.'))
+    icao = models.CharField(max_length=4, null=True, blank=True)
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
+    altitude = models.FloatField()
+    timezone_offset = models.FloatField()
+    dst = models.CharField(max_length=1)
+    timezone = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.airport_name
@@ -21,7 +35,13 @@ class Airline(AbstractBase):
     These is an airline to which a plane can belong to such as
     KLM
     """
-    name = models.CharField(max_length=255)
+    airline_name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255, null=True, blank=True)
+    iata = models.CharField(max_length=2, null=True, blank=True)
+    icao = models.CharField(max_length=3, null=True, blank=True)
+    callsign = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    active = models.CharField(max_length=1, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -82,3 +102,31 @@ class TravelClassSeatCapacity(AbstractBase):
 
     def __str__(self):
         return self.seat_capacity
+
+
+class Route(AbstractBase):
+    """
+    Stores details about a particular route
+    """
+
+    airline = models.ForeignKey(Airline)
+    source_airport = models.ForeignKey(Airport, related_name='source_airport')
+    destination_airport = models.ForeignKey(
+        Airport, related_name='destination_airport')
+    codeshare = models.CharField(
+        max_length=1, null=True,
+        help_text=('A flight is a codeshare if not operated by Airline,'
+                   'but another carrier'))
+    equipment = models.CharField(
+        max_length=3, help_text='3-letter codes for plane type')
+    stops = models.IntegerField()
+
+
+class RouteTree(MPTTModel, AbstractBase):
+    """
+    This are the allowed routes by an airline it uses MPTT
+    """
+    airline = models.ForeignKey(Airline)
+    airport = models.ForeignKey(Airport)
+    parent = TreeForeignKey(
+        'self', null=True, blank=True, related_name='children', db_index=True)
