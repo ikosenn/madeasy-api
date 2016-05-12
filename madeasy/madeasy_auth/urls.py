62f47200bc7dbe3659e4e3ae8d60ec718259b74e
Ian Kosen <i@ikosenn.me>
2016-05-13 01:24:15 +0300

    Create special end point to sign up users (#23)

--- madeasy/madeasy_auth/urls.py
+++ madeasy/madeasy_auth/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls import url
 from rest_framework import routers
 
 from .views import (
     UserViewSet,
     UserProfileViewSet,
     OauthApplicationViewSet,
+    CreateUserView,
 )
 
 
@@ -13,3 +15,7 @@ router.register(r'user_profiles', UserProfileViewSet)
 router.register(r'applications', OauthApplicationViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    url(r'^create_user/$', CreateUserView.as_view(), name="create_user"),
+]
--- madeasy/madeasy_auth/views.py
+++ madeasy/madeasy_auth/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
 from .models import (
     User,
     UserProfile,
@@ -16,6 +17,13 @@ from .serializers import (
 class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+    permission_classes = (AllowAny,)
+
+    queryset = None
     serializer_class = UserSerializer
 
 
